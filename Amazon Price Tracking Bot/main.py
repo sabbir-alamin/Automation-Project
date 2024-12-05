66d6a26407e342e3c084e7ffa4e105bafbc25b95
sabbir <sabbiralamin707@gmail.com>
2024-12-05 12:34:31 +0600

    updated code and added some comments

--- Amazon Price Tracking Bot/main.py
+++ Amazon Price Tracking Bot/main.py
@@ -3,45 +3,54 @@ from bs4 import BeautifulSoup
 import lxml
 import smtplib
 
-MAIL = "rebillion.bir@gmail.com"
-PASSWORD = "ucon gdct xtbg muwx"
+# Email credentials for sending alerts
+MAIL = "YOUR MAIL"
+PASSWORD = "YOUR PASSWORD"
 
+# URL of the Amazon product to monitor
 URL = "https://www.amazon.com/dp/B075CWJ3T8?ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6&th=1"
 
+# HTTP headers to mimic a legitimate browser request
 USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"
 ACCEPT_LANGUAGE = "en-US,en;q=0.9"
 ACCEPT_ENCODING = "gzip, deflate, br, zstd"
 UPGRADE_INSECURE_REQUESTS = "1"
 ACCEPT = "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7"
-X_HTTPS = "on"
 
+# Headers dictionary for the GET request
 header = {
-
     "User-Agent": USER_AGENT,
     "Accept-Language": ACCEPT_LANGUAGE,
     "Accept-Encoding": ACCEPT_ENCODING,
     "Upgrade-Insecure-Requests": UPGRADE_INSECURE_REQUESTS,
     "Accept": ACCEPT,
-
 }
 
+# Send an HTTP GET request to the product page
 response = requests.get(url=URL, headers=header)
 
+# Parse the HTML content using BeautifulSoup with the lxml parser
 soup = BeautifulSoup(response.content, "lxml")
-# print(soup.prettify())
 
+# Extract the product price from the HTML using its class name
 price = soup.find(class_="a-offscreen").get_text()
+
+# Transforming the price into a numeric value
 price_without_currency = price.split("$")[1]
 price_as_float = float(price_without_currency)
 
+# Check if the price is below a certain threshold and send mail
 if price_as_float < 200:
     with smtplib.SMTP("smtp.gmail.com") as connection:
         connection.starttls()
         connection.login(user=MAIL, password=PASSWORD)
-        connection.sendmail(from_addr=MAIL,
-                            to_addrs="sabbiralamin707@gmail.com",
-                            msg=f"Subject:Amazon Price Alter!! \n\nInstant Pot Duo Plus 9-in-1 Electric Pressure "
-                                f"Cooker, Slow Cooker, Rice Cooker, Steamer, Sauté, Yogurt Maker, "
-                                f"Warmer & Sterilizer, Includes App With Over 800 Recipes, Stainless Steel, "
-                                f"8 Quart is now {price_as_float}$".encode("utf-8")
-                            )
+        connection.sendmail(
+            from_addr=MAIL,
+            to_addrs="sabbiralamin707@gmail.com",
+            msg=(
+                f"Subject:Amazon Price Alert!! \n\n"
+                f"Instant Pot Duo Plus 9-in-1 Electric Pressure Cooker, Slow Cooker, Rice Cooker, Steamer, "
+                f"Sauté, Yogurt Maker, Warmer & Sterilizer, Includes App With Over 800 Recipes, "
+                f"Stainless Steel, 8 Quart is now {price_as_float}$"
+            ).encode("utf-8")
+        )
--- Form Fill Up Automation/Form Fill up Automation.py
+++ Form Fill Up Automation/Form Fill up Automation.py
@@ -1,23 +1,32 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+
 URL = "http://secure-retreat-92358.herokuapp.com/"
 
+# Prevent the browser from closing automatically after execution
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 
+# Initialize the Chrome WebDriver with the specified options
 driver = webdriver.Chrome(options=chrome_options)
 
+# Open the specified web page in the browser
 driver.get(URL)
 
+# Locate the "First Name" input field using its 'name' attribute and input a value
 first_name = driver.find_element(By.NAME, "fName")
 first_name.send_keys("Sabbir")
 
+# Locate the "Last Name" input field using its 'name' attribute and input a value
 last_name = driver.find_element(By.NAME, "lName")
 last_name.send_keys("Al-Amin")
 
+# Locate the "Email" input field using its 'class' attribute and input a value
 email = driver.find_element(By.CLASS_NAME, "bottom")
 email.send_keys("alamin170@gmail.com")
 
+# Locate the "Submit" button using its CSS selector and simulate a click action
 button_click = driver.find_element(By.CSS_SELECTOR, ".btn-primary")
 button_click.click()
+
